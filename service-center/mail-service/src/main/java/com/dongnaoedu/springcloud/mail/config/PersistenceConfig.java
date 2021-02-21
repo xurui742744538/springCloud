152a59c6fdc660a345636ec20eca9b5c1fe52940
xulinfei <xulinfei@ikingtec.com>
2021-02-21 12:56:04 +0800

    启动项目指定数据库方言

---
+++ service-center/mail-service/src/main/java/com/dongnaoedu/springcloud/mail/config/PersistenceConfig.java
@@ -0,0 +1,29 @@
+package com.dongnaoedu.springcloud.mail.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
+
+/**
+ * @author xurui
+ */
+@Configuration
+public class PersistenceConfig {
+
+    @Bean(name = "entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        return new LocalSessionFactoryBean();
+    }
+    @Bean
+    public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+        HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
+        factory.setEntityManagerFactory(emf);
+        return factory;
+    }
+
+
+}
+
